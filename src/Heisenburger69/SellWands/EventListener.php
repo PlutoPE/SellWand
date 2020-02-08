aa736c7281e2ab75397df300d89806d9e8ad81bb
mikeygio10 <46503241+mikeygio10@users.noreply.github.com>
2020-02-07 22:22:29 -0500

    Create EventListener.php

---
+++ src/Heisenburger69/SellWands/EventListener.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Heisenburger69\SellWands;
+
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\Listener;
+
+class EventListener implements Listener {
+
+    public function onBlockBreak(BlockBreakEvent $event) {
+        if($event->isCancelled()) {
+            return;
+        }
+        **Idk what to do here ani help me pls Thx**
